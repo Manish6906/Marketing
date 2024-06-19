e30d57c5ba35e7982d6ff424a98b3eba1f3609a8
Manish6906 <20-m-0742@rayatbahrauniversity.edu.in>
2024-06-19 10:02:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className='bg-yellow-400'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum illo, ab a necessitatibus eos error ratione maxime commodi autem eaque optio, nesciunt saepe molestiae quod doloribus modi neque, fuga recusandae?
+    Ullam non placeat autem. Neque quas id amet fuga perferendis molestias nemo corporis eius! Nostrum, id fuga. Id aperiam itaque quas nobis illo quo reiciendis maxime blanditiis facilis? Voluptas, doloribus.</p>
+    </>
+  )
+}
+
+export default App
